d7cf8bf8c19c528e72d159c1816ddbe178bee615
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2021-12-27 00:22:49 +0530

    Added create and update expense APIs

--- expenzo/expense/urls.py
+++ expenzo/expense/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GetAllExpensesView
+from .views import GetAllExpensesView, CreateExpenseView, UpdateExpenseView
 
 urlpatterns = [
-    path('get-expenses', GetAllExpensesView.as_view(), name='get-expenses')
+    path('get-expenses', GetAllExpensesView.as_view(), name='get-expenses'),
+    path('create-expense', CreateExpenseView.as_view(), name='create-expense'),
+    path('update-expense', UpdateExpenseView.as_view(), name='update-expense')
 ]
