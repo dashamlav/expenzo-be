c8c933c3cf2d2fdee4b42fe4ac1db57633c75530
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2021-12-28 01:16:16 +0530

    Changed serializer

--- expenzo/expense/views.py
+++ expenzo/expense/views.py
@@ -4,24 +4,24 @@ from rest_framework.generics import GenericAPIView, ListAPIView, CreateAPIView, 
 from rest_framework.mixins import UpdateModelMixin
 from .models import Expense
 from rest_framework.permissions import IsAuthenticated
-from .serializers import ListExpenseSerializer, CreateOrUpdateExpenseSerializer
+from .serializers import ExpenseSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
 
 class GetAllExpensesView(ListAPIView):
-    serializer_class = ListExpenseSerializer
+    serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
 
     def get_queryset(self):
         user = self.request.user
-        return self.queryset.filter(appUser=user)
+        return self.queryset.filter(appUser=user).order_by('-date')
     
 
 class CreateExpenseView(CreateAPIView):
-    serializer_class = CreateOrUpdateExpenseSerializer
+    serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
     queryset = Expense.objects.all()
     
@@ -30,7 +30,7 @@ class CreateExpenseView(CreateAPIView):
 
 
 class UpdateExpenseView(GenericAPIView, UpdateModelMixin):
-    serializer_class = CreateOrUpdateExpenseSerializer
+    serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
@@ -41,6 +41,7 @@ class UpdateExpenseView(GenericAPIView, UpdateModelMixin):
             'appUser': self.request.user,
             'id' : self.request.data.get('id', None)
         }
+        print(filter_kwargs)
         return get_object_or_404(Expense, **filter_kwargs)
 
 
