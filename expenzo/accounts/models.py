982eb8f7aeceb1de6b66593a419f3ac3b8bfd4d8
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2022-01-08 15:27:52 +0530

    Added db indexing

--- expenzo/accounts/models.py
+++ expenzo/accounts/models.py
@@ -7,7 +7,7 @@ class AppUser(AbstractUser):
     #first_name, last_name, password, is_staff, is_active is handled in AbstractUser.
 
     username = models.CharField(max_length=50, null=False, blank=False, default='user')
-    email = models.EmailField(null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True, db_index=True)
     userType = models.CharField(max_length=3, choices=UserType.Choices, default=UserType.DEFAULT, null=False, blank=False)
     defaultCurrency = models.CharField(max_length=3, choices=CurrencyType.Choices, null=False, blank=False, default=CurrencyType.INR)
     
