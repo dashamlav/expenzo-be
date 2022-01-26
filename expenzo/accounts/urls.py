5780d16f559238774248586dabe3f9000b32ae65
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2022-01-27 00:44:14 +0530

    Generate and validate OTP view for registration

--- expenzo/accounts/urls.py
+++ expenzo/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, RegisterView, ValidateTokenView
+from .views import LoginView, RegisterView, ValidateTokenView, RegisterPreflightView
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('register', RegisterView.as_view(), name='register'),
+    path('register-preflight', RegisterPreflightView.as_view(), name='register-preflight'),
     path('validate-token', ValidateTokenView.as_view(), name='validate-token'),
 ]
