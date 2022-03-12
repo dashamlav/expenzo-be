888dafb682470419dcd58439cbe7478a13fc0dee
Abhishek Thaker <abhishek.thaker@siftconsultancy.com>
2022-03-13 00:00:33 +0530

    dockerize app changes

--- expenzo/start_expenzo_server.sh
+++ expenzo/start_expenzo_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 export PROJECT_PATH=`pwd`
+
+echo "Making migrations..."
+python manage.py migrate --no-input
+
+echo "Starting expenzo django server..."
 uwsgi --static-map /static=$PROJECT_PATH/static --ini $PROJECT_PATH/uwsgi.ini
